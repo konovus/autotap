a58cecd2f978a4d1322ccf349e84ee8b76c7e982
Constantin Cioban <ciobancostea@gmail.com>
2020-09-20 17:07:04 +0100

    testing motion control

--- app/src/main/java/com/konovus/autotap/MainActivity.java
+++ app/src/main/java/com/konovus/autotap/MainActivity.java
@@ -1,16 +1,45 @@
 package com.konovus.autotap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout constraintLayout;
+    TextView hello;
+    private float x;
+    private float y;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("altceva");
+        constraintLayout = findViewById(R.id.generalLayout);
+        hello = findViewById(R.id.hello);
+
+        constraintLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                x = event.getX();
+                y = event.getY();
+
+                if(event.getAction() == MotionEvent.ACTION_MOVE){
+                    hello.setX(x - hello.getWidth()/2);
+                    hello.setY(y - hello.getHeight());
+                }
+                return true;
+            }
+        });
+
+
+
     }
 }
