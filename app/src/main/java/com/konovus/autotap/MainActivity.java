5e7050559288e7fb8d5332763952076dd862a878
Constantin Cioban <ciobancostea@gmail.com>
2020-09-21 09:48:06 +0100

    working floating btn example

--- app/src/main/java/com/konovus/autotap/MainActivity.java
+++ app/src/main/java/com/konovus/autotap/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +18,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ConstraintLayout constraintLayout;
-    TextView hello;
-    Button do_click;
-    Button btn_first;
-    private float x = 500;
-    private float y = 500;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -28,58 +25,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        constraintLayout = findViewById(R.id.generalLayout);
-        hello = findViewById(R.id.hello);
-        do_click = findViewById(R.id.do_click);
-        btn_first = findViewById(R.id.btn_first);
+        Bundle bundle = getIntent().getExtras();
 
+        if(bundle != null && bundle.getString("LAUNCH").equals("YES")) {
+            startService(new Intent(MainActivity.this, FlyBitch.class));
+        }
 
-        constraintLayout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                x = event.getRawX();
-                y = event.getRawY();
+        Button launch = (Button)findViewById(R.id.start);
+        launch.setOnClickListener(new View.OnClickListener() {
 
-                if(event.getAction() == MotionEvent.ACTION_MOVE){
-                    hello.setX(x - hello.getWidth()/2);
-                    hello.setY(y - hello.getHeight());
-                }
-                return true;
-            }
-        });
-
-
-        btn_first.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_DOWN)
-                    Toast.makeText(MainActivity.this, "Btn first tapped", Toast.LENGTH_LONG).show();
-
-                return true;
-            }
-        });
-        do_click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hello.setVisibility(View.GONE);
-                long downTime = SystemClock.uptimeMillis();
-                long eventTime = SystemClock.uptimeMillis() + 100;
-// List of meta states found here: developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
-                int metaState = 0;
-                System.out.println("X, Y -- " + x + ", " + y);
-                MotionEvent motionEvent = MotionEvent.obtain(
-                        downTime,
-                        eventTime,
-                        MotionEvent.ACTION_DOWN,
-                        x,
-                        y,
-                        metaState
-                );
-                ViewGroup viewGroup = constraintLayout;
-                viewGroup.dispatchTouchEvent(motionEvent);
+                startService(new Intent(MainActivity.this, FlyBitch.class));
             }
         });
 
+        Button stop = (Button)findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, FlyBitch.class));
+            }
+        });
+
+        if (!Settings.canDrawOverlays(getApplicationContext())) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 0);
+        }
     }
 
 }
